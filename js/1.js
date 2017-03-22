c319f2607299121b3c31f8e05353e0d74a5ba94e
anishCFC <anishreddy454@gmail.com>
2017-03-22 22:32:04 +0530

    changed the style of page

--- js/1.js
+++ js/1.js
@@ -1,1 +1,51 @@
+$(document).ready(function () {
+      var r = 64;
+      var consize = 600;
+      var gridmaker = function(x){
+        var i=0;
+        var cells = x*x;
+        while(i<cells){
+          $('.container').append('<div class="pixel"></div>');
+          i++;
+        }
+        $('.pixel').css({
+          "height": consize/r + "px",
+          "width": consize/r + "px",
+        });
+      };
 
+      var paint = function(y){
+        $(y).mouseenter(function() {
+          $(this).css("background-color","#000000");
+        });
+      };
+
+      gridmaker(r);
+      paint('.pixel');
+
+      var clear = function(){
+        $('#but').click(function() {
+          $('.pixel').remove();
+          gridmaker(r);
+          paint('.pixel');
+        });
+      };
+
+      clear();
+
+      var changeresol = function(){
+        $('#but1').click(function() {
+          r = prompt('enter no of rows');
+          if(r>150||r<5){
+            alert('input out of range');
+            r = 75;
+          }
+          $('.pixel').remove();
+          gridmaker(r);
+          paint('.pixel');
+        });
+      };
+
+      changeresol();
+
+});
